7cb1a86f90d3edd52c4f61e6d29c949786dd68e2
kght6123 <i.feeling.song+github@gmail.com>
2024-08-17 02:34:36 +0900

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import './App.css'
 
 import CustomButton from './components/introduction/CustomButton'
 import CustomComboBox from './components/introduction/CustomComboBox'
+import CustomComboBox2 from './components/introduction/CustomComboBox2'
 
 function App() {
   return (
     <>
       <CustomButton />
       <CustomComboBox />
+      <CustomComboBox2 />
     </>
   )
 }
